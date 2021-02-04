ca1a1c2724ea935cf7c087f3b98f7a7a6e28d581
Chien DUONG <chienduong90@gmail.com>
2021-02-04 10:56:50 +0100

    java design pattern abstract factory

---
+++ src/HDFC.java
@@ -0,0 +1,12 @@
+class HDFC implements Bank{
+
+    private final String BNAME;
+    public HDFC() {
+        BNAME="HDFC BANK";
+    }
+
+    @Override
+    public String getBankName() {
+        return BNAME;
+    }
+}
---
+++ src/ICICI.java
@@ -0,0 +1,9 @@
+class ICICI implements Bank{
+    private final String BNAME;
+    public ICICI () {
+        BNAME="ICICI BANK";
+    }
+    public String getBankName() {
+        return BNAME;
+    }
+}
